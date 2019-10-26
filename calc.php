bfe3e8d9a21663960f6876e01d14cc48e90fd477
Brian Gomez <briangx92@gmail.com>
2019-10-26 09:54:27 -0400

    Form for inputting values

--- calc.php
+++ calc.php
@@ -1,4 +1,11 @@
 <?php
+session_start();
+
+
+$_SESSION['correct'];
+$_SESSION['attempt'];
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,6 +16,22 @@
     <title>Calculator</title>
 </head>
 <body>
-    
+    <h1>Math Exercises</h1>
+    <form action="calc.php" method="post">
+        <input type="text" name="num1" placeholder="Number 1">
+        <input type="text" name="num2" placeholder="Number 2">
+        <select name="operator" id="">
+            <option value="">None</option>
+            <option value="">Add</option>
+            <option value="">Subtract</option>
+            <option value="">Multiply</option>
+            <option value="">Divide</option>
+        </select>
+        <br>
+        <button type="submit" name="submit" value="submit">Calculate</button>
+
+
+
+    </form>
 </body>
 </html>
