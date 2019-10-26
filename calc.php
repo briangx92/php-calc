96e42a9f574a52550887bd82cd427dce615a25c2
Brian Gomez <briangx92@gmail.com>
2019-10-26 14:11:29 -0400

    Added a function that does the calculations of the answer. Going to make a input for the user to input their answer.

--- calc.php
+++ calc.php
@@ -1,17 +1,49 @@
 <?php
-if (isset($_GET['range'])) {
-    $x1 = $_GET['x1'];
-    $x2 = $_GET['x2'];
-    $y1 = $_GET['y1'];
-    $y2 = $_GET['y2'];
+if (isset($_POST['range'])) {
+    $x1 = $_POST['x1'];
+    $x2 = $_POST['x2'];
+    $y1 = $_POST['y1'];
+    $y2 = $_POST['y2'];
     $x_rand = rand((int)$x1,(int)$x2);
     $y_rand = rand((int)$y1,(int)$y2);
-    echo $x_rand;
-    echo $y_rand;
+    $x = $x_rand;
+    $y = $y_rand;
+    echo "{$x}<br>";
+    echo $y;
 
 }
-@$x_rand = $x;
-@$y_rand = $y;
+
+
+$sub = isset($_POST['submit']);
+
+function calculate($sub) {
+    if ($sub) {
+    $result1 = $_POST['num1'];
+    $result2 = $_POST['num2'];
+    $operator = $_POST['operator'];
+    switch ($operator) {
+        case "Select":
+            echo "Select an operator!";
+    }
+        if ($operator == "+") {
+            $total = $result1 + $result2;
+        }
+        if ($operator == "-") {
+            $total = $result1 - $result2;
+        }
+        if ($operator == "*") {
+            $total = $result1 * $result2;
+        }
+        if ($operator == "/") {
+            $total = $result1 / $result2;
+        }
+        echo "What is {$result1} {$operator} {$result2}?";
+    
+
+    }
+}
+$calculate = calculate($sub);
+
 
 ?>
 
@@ -25,7 +57,8 @@ if (isset($_GET['range'])) {
 </head>
 <body>
     <h1>Math Exercises</h1>
-    <form action="calc.php" method="get">
+    <form action="calc.php" method="POST">
+        <!-- Range Form -->
         <fieldset>
             <legend>Range</legend>
             <input type="text" name="x1" placeholder="X Range 1">
@@ -38,15 +71,16 @@ if (isset($_GET['range'])) {
         </fieldset>
         
     </form>
-    <form action="calc.php" method="get">
+    <!-- Answer Form -->
+    <form action="calc.php" method="POST">
         <fieldset>
             <legend>Enter Answer</legend>
-            <input type="text" name="num1" placeholder="Number 1">
+            <input type="text" name="num1" value="<?php echo @$x; ?>" placeholder="Number 1">
             <br>
-            <input type="text" name="num2" placeholder="Number 2">
+            <input type="text" name="num2" value="<?php echo @$y; ?>" placeholder="Number 2">
             <select name="operator">
                 <option>Select</option>
-                <option>Add</option>
+                <option value="+">Add</option>
                 <option>Subtract</option>
                 <option>Multiply</option>
                 <option>Divide</option>
@@ -59,35 +93,6 @@ if (isset($_GET['range'])) {
         
      
     </form>
-    <p>The answer is: </p>
-    <?php
-    if (isset($_GET['submit'])) {
-        $result1 = $_GET['num1'];
-        $result2 = $_GET['num2'];
-        $operator = $_GET['operator'];
-        switch ($operator) {
-            case "Select":
-                echo "Select an operator!";
-            break;
-            case "Add":
-                echo $result1 + $result2;
-            break;
-            case "Subtract":
-                echo $result1 - $result2;
-            break;
-            case "Multiply":
-                echo $result1 * $result2;
-            break;
-            case "Divide":
-                echo $result1 / $result2;
-            break;
-
-        }
-        
-
-    }
-print_r($_GET);
-?>
 
 </body>
 </html>
